2cc1725826d74afd9e634216a7e94c074b1e0a32
a-yuto <arakiyutoam@gmail.com>
2020-02-06 11:05:23 +0000

    mean

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ extern crate rand;
 use rand::Rng;
 
 use std::fs::File;
-/*colu に対してpushとか色々関数を定義するべき*/
-trait ColumFunction<T> {
-    fn print_colum(col:&Colum<T>);
+fn print_typename<T>(_: T) {
+    println!("{}", std::any::type_name::<T>());
 }
+
 struct Colum<T> {
     name: String,
     data: Vec<T>,
@@ -23,17 +23,14 @@ fn print_stringcolum(col: &Colum<String>) {
 
 
 pub struct IrisDf {
-    itr: Colum<usize>,
+    itr:         Colum<usize>,
     sepal_length: Colum<f64>, 
     sepal_width:  Colum<f64>, 
-    petal_length: Colum<f64>,
-    petal_width:  Colum<f64>, 
-    species:      Colum<String>,
+    petal_length:Colum<f64>,
+    petal_width: Colum<f64>, 
+    species:     Colum<String>,
 }
 
-fn print_typename<T>(_: T) {
-    println!("{}", std::any::type_name::<T>());
-}
 fn print_irisdf(df: &IrisDf) {
     let length = df.itr.data.len();;
     println!("{}:{}:{}:{}:{}:`{}",
@@ -50,8 +47,17 @@ fn data_size(data: &IrisDf) -> (usize,usize) {
     (tate,yoko)
 }
 
-fn sep_tt(data: &IrisDf) {
+fn sep_tt(data: &IrisDf) -> (IrisDf,IrisDf){
     let (_tate,_yoko) = data_size(&data);
+    
+    let mut _test_itr: Colum<usize> = Colum {
+        name: "sepal_length".to_string(),
+        data: Vec::new()
+    };
+    let mut _train_itr: Colum<usize> = Colum {
+        name: "sepal_length".to_string(),
+        data: Vec::new()
+    };
     let mut _test_sepal_length: Colum<f64> = Colum {
         name: "sepal_length".to_string(),
         data: Vec::new()
@@ -60,6 +66,34 @@ fn sep_tt(data: &IrisDf) {
         name: "sepal_length".to_string(),
         data: Vec::new()
     };
+    
+    let mut _test_sepal_width: Colum<f64> = Colum {
+        name: "sepal_width".to_string(),
+        data: Vec::new()
+    };
+    let mut _train_sepal_width: Colum<f64> = Colum {
+        name: "sepal_width".to_string(),
+        data: Vec::new()
+    };
+    
+    let mut _test_petal_length: Colum<f64> = Colum {
+        name: "petal_length".to_string(),
+        data: Vec::new()
+    };
+    let mut _train_petal_length: Colum<f64> = Colum {
+        name: "petal_length".to_string(),
+        data: Vec::new()
+    };
+    
+    let mut _test_petal_width: Colum<f64> = Colum {
+        name: "petal_width".to_string(),
+        data: Vec::new()
+    };
+    let mut _train_petal_width: Colum<f64> = Colum {
+        name: "petal_width".to_string(),
+        data: Vec::new()
+    };
+    ;
     let mut _test_species: Colum<String> = Colum {
         name: "species".to_string(),
         data: Vec::new()
@@ -71,20 +105,50 @@ fn sep_tt(data: &IrisDf) {
     for itr in 0.._tate {
         let mut    rng      = rand::thread_rng();
         let random_num: i32 = rng.gen();
+        let         _itr = data.itr.data[itr];
         let sepal_length = data.sepal_length.data[itr];
+        let sepal_width  = data.sepal_width.data[itr];
+        let petal_length = data.sepal_length.data[itr];
+        let petal_width  = data.sepal_width.data[itr];
         let      species = &data.species.data[itr];
         if random_num % 2 == 0 {
+            _test_itr         .data.push(_itr);
             _test_sepal_length.data.push(sepal_length);
-            _test_species.data.push(species.to_string());
+            _test_sepal_width .data.push(sepal_width);
+            _test_petal_length.data.push(petal_length);
+            _test_petal_width .data.push(petal_width);
+            _test_species     .data.push(species.to_string());
         } else {
+            _train_itr         .data.push(_itr);
             _train_sepal_length.data.push(sepal_length);
-            _train_species.data.push(species.to_string());
+            _train_sepal_width .data.push(sepal_width);
+            _train_petal_length.data.push(petal_length);
+            _train_petal_width .data.push(petal_width);
+            _train_species     .data.push(species.to_string());
         }
     }
-    print_colum(&_test_sepal_length);
-    print_stringcolum(&_test_species);
-    print_colum(&_train_sepal_length);
-    print_stringcolum(&_train_species);
+    (IrisDf {
+        itr:          _test_itr,
+        sepal_length: _test_sepal_length,
+        sepal_width:  _test_sepal_width,
+        petal_length: _test_petal_length,
+        petal_width:  _test_petal_width,
+        species:      _test_species,
+    },
+    IrisDf {
+        itr:          _train_itr,
+        sepal_length: _train_sepal_length,
+        sepal_width:  _train_sepal_width,
+        petal_length: _train_petal_length,
+        petal_width:  _train_petal_width,
+        species:      _train_species,
+    })
+}
+
+pub struct IrisSta {
+    sentosa   : f64,
+    versicolor: f64,
+    virginica : f64
 }
 
 fn main() {
@@ -139,5 +203,27 @@ fn main() {
         petal_width:  _petal_width,
         species:      _species,    
     };
-    sep_tt(&data);
+    let (test,train) = sep_tt(&data);
+    print_irisdf(&test);
+    println!("");
+    print_irisdf(&train);
+    
+    let (test_tate,test_yoko) = data_size(&test);
+    let mut _mean = IrisSta {
+        sentosa:    0.0,
+        versicolor: 0.0,
+        virginica:  0.0
+    };
+    for i in (0..test_tate) {
+        if &test.species.data[i] == "setosa" {
+            _mean.sentosa   += test.sepal_width.data[i];
+        } else if &test.species.data[i] == "versicolor"{
+            _mean.versicolor += test.sepal_width.data[i];
+        } else {
+            _mean.virginica  += test.sepal_width.data[i];
+        }
+    }
+    println!("sntosa's sepal width is {}"    ,_mean.sentosa);
+    println!("versicolor's sepal width is {}",_mean.versicolor);
+    println!("viginica's sepal width is {}"  ,_mean.virginica);
 }
