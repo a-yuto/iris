900a4dc0743801659e5c1c395d7d35d511eb622b
a-yuto <arakiyutoam@gmail.com>
2020-02-06 12:59:03 +0000

    make mean predict

--- src/main.rs
+++ src/main.rs
@@ -32,10 +32,10 @@ pub struct IrisDf {
 }
 
 fn print_irisdf(df: &IrisDf) {
-    let length = df.itr.data.len();;
+    let length = df.itr.data.len();
     println!("{}:{}:{}:{}:{}:`{}",
     df.itr.name,df.sepal_length.name,df.sepal_width.name,df.petal_length.name,df.petal_width.name,df.species.name);
-    for i in (0..length) {
+    for i in 0..length {
         println!("{:?}:  {:?}:          {:?}:        {:?}:        {:?}:      {:?}",
         df.itr.data[i],df.sepal_length.data[i],df.sepal_width.data[i],df.petal_length.data[i],df.petal_width.data[i],df.species.data[i]);
     }
@@ -51,11 +51,11 @@ fn sep_tt(data: &IrisDf) -> (IrisDf,IrisDf){
     let (_tate,_yoko) = data_size(&data);
     
     let mut _test_itr: Colum<usize> = Colum {
-        name: "sepal_length".to_string(),
+        name: "itr".to_string(),
         data: Vec::new()
     };
     let mut _train_itr: Colum<usize> = Colum {
-        name: "sepal_length".to_string(),
+        name: "itr".to_string(),
         data: Vec::new()
     };
     let mut _test_sepal_length: Colum<f64> = Colum {
@@ -93,7 +93,7 @@ fn sep_tt(data: &IrisDf) -> (IrisDf,IrisDf){
         name: "petal_width".to_string(),
         data: Vec::new()
     };
-    ;
+    
     let mut _test_species: Colum<String> = Colum {
         name: "species".to_string(),
         data: Vec::new()
@@ -151,6 +151,59 @@ pub struct IrisSta {
     virginica : f64
 }
 
+fn mean_predict(df: &IrisDf) -> Box<Fn(f64) -> String> {
+    let (_tate,_yoko) = data_size(&df);
+    println!("test_tate = {}",_tate);
+    let mut _sepal_width_mean = IrisSta {
+        sentosa:    0.0,
+        versicolor: 0.0,
+        virginica:  0.0
+    };
+    let mut _species_num = IrisSta {
+        sentosa:    0.0,
+        versicolor: 0.0,
+        virginica:  0.0
+    };
+    for i in 0.._tate {
+        if &df.species.data[i] == "setosa" {
+            _sepal_width_mean.sentosa += df.sepal_width.data[i];
+            _species_num.sentosa      += 1.0;
+        } else if &df.species.data[i] == "versicolor"{
+            _sepal_width_mean.versicolor += df.sepal_width.data[i];
+            _species_num.versicolor      += 1.0;
+        } else {
+            _sepal_width_mean.virginica  += df.sepal_width.data[i];
+            _species_num.virginica       += 1.0;
+        }
+    }
+    _sepal_width_mean.sentosa    /= _species_num.sentosa;
+    _sepal_width_mean.versicolor /= _species_num.versicolor;
+    _sepal_width_mean.virginica  /= _species_num.virginica;
+    let bound_ver_vig = (_sepal_width_mean.sentosa    + _sepal_width_mean.versicolor) / 2.0;    
+    let bound_vig_sen = (_sepal_width_mean.versicolor + _sepal_width_mean.virginica ) / 2.0;
+    Box::new(move |_sepal_width|
+        if _sepal_width < bound_ver_vig {
+            "versicolor".to_string()
+        } else if _sepal_width < bound_vig_sen {
+            "virginica".to_string()
+        }else {
+            "setosa".to_string()
+        }
+    )
+}
+
+fn correct_ans_rate(test: &IrisDf, predict: Box<Fn(f64) -> String>) -> f64 {
+    let mut correct_num = 0.0;
+    let (_tate,_yoko) = data_size(&test);
+    for i in 0.._tate {
+        let pre_result = predict(test.sepal_width.data[i]);
+        let answer     = &test.species.data[i];
+        println!("predict result is {}, answer is {}",pre_result,answer);
+        if &pre_result == answer {correct_num += 1.0;}
+    }
+    100.0 * (correct_num / _tate as f64)
+}
+
 fn main() {
     let path = "test_data.csv";
     let mut _table = csv::Reader::from_path(path).unwrap();   
@@ -179,7 +232,6 @@ fn main() {
         name: "species".to_string(),
         data: Vec::new()
     };
-    println!("{:?}",_table.headers());
     for _line in _table.records() {
         let mut cnt_yoko = 0;
         for _val in _line.unwrap().iter() {
@@ -204,26 +256,6 @@ fn main() {
         species:      _species,    
     };
     let (test,train) = sep_tt(&data);
-    print_irisdf(&test);
-    println!("");
-    print_irisdf(&train);
-    
-    let (test_tate,test_yoko) = data_size(&test);
-    let mut _mean = IrisSta {
-        sentosa:    0.0,
-        versicolor: 0.0,
-        virginica:  0.0
-    };
-    for i in (0..test_tate) {
-        if &test.species.data[i] == "setosa" {
-            _mean.sentosa   += test.sepal_width.data[i];
-        } else if &test.species.data[i] == "versicolor"{
-            _mean.versicolor += test.sepal_width.data[i];
-        } else {
-            _mean.virginica  += test.sepal_width.data[i];
-        }
-    }
-    println!("sntosa's sepal width is {}"    ,_mean.sentosa);
-    println!("versicolor's sepal width is {}",_mean.versicolor);
-    println!("viginica's sepal width is {}"  ,_mean.virginica);
+    let predict = mean_predict(&train);
+    println!("predict result is {}",correct_ans_rate(&test,predict));
 }
